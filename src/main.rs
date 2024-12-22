8c68fc5af36380baac3ac9decb36ec6af5c6330c
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2024-12-22 12:05:00 +0530

    feat!: NUKE

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use std::net::TcpListener;
-
-use z2p::run;
-
-#[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to port 8000");
-    run(listener)?.await
-}
